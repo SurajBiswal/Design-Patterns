d49337c49bdee47f37e1adab1ac14b393e9b9726
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2025-03-23 23:26:25 +0530

    "starategy"

--- src/Singleton/Singleton1.java
+++ src/Singleton/Singleton1.java
@@ -1,5 +1,12 @@
 package Singleton;
 
 public class Singleton1 {
+    private static final Singleton1 INSTANCE = new Singleton1(); // Instance created at class loading
+
+    private Singleton1() {} // Private constructor prevents instantiation
+
+    public static Singleton1 getInstance() {
+        return INSTANCE;
+    }
 
 }
